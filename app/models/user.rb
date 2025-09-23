8a41e7a8f98d5413cc00f61282114ef2d6c3722c
Dhiogoandre <dhiogoandre26@gmail.com>
2025-09-22 23:51:52 -0400

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  belongs_to :a_cargo
+  belongs_to :a_unidade
+  belongs_to :a_tipo_usuario
+  belongs_to :a_status
+
+  validates :nome, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :telefone, presence: true
+  validates :cpf, presence: true, uniqueness: true
+  
+        
 end
