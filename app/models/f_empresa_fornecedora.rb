1204c792f1b02c27987e870e8b5ff4ef16f1d23c
Dhiogoandre <dhiogoandre26@gmail.com>
2025-09-26 18:57:00 -0400

    WIP: empresas fornecedoras

--- app/models/f_empresa_fornecedora.rb
+++ app/models/f_empresa_fornecedora.rb
@@ -2,4 +2,9 @@
 
 class FEmpresaFornecedora < ApplicationRecord
   # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
+  belongs_to :a_status
+  belongs_to :g_municipio
+
+  validates :nome_fantasia, presence: true
+  validates :razao_social, presence: true
 end
